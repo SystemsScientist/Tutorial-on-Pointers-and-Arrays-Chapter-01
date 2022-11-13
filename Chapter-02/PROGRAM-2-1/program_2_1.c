1b4ab2a210dca9b9c33841b6066070cd60b8cacb
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-13 06:27:19 -0600

    Updating Program 2.1

--- Chapter-02/PROGRAM-2-1/program_2_1.c
+++ Chapter-02/PROGRAM-2-1/program_2_1.c
@@ -14,7 +14,7 @@ int main(void) {
 	printf("\n");
 	for (i = 0; i < 6; i++) {
 		printf("my_array[%d] = %d   ", i, my_array[i]); /* <-- A */
-		printf("ptr + %d = %d\n", i, *(ptr + 1));   	/* <-- B */
+		printf("ptr + %d = %d\n", i, *(ptr + i));   	/* <-- B */
 	}
 
 	printf("\n");
