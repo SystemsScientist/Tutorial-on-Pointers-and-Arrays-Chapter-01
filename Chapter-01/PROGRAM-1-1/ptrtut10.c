3d5c8293c4ed7978b39d4d279f9924c869187681
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-11-13 05:55:56 -0600

    Adding Program 1.1

---
+++ Chapter-01/PROGRAM-1-1/ptrtut10.c
@@ -0,0 +1,25 @@
+// ptrtut10.c -- illustrates a pointer
+
+#include <stdio.h>
+
+int j, k;
+int *ptr;
+
+int main(void) {
+
+	j = 1;
+	k = 2;
+
+	ptr = &k;
+	printf("\n");
+	printf("j has the value %d and is stored at %p\n", 
+		j, (void *)&j);
+	printf("k has the value %d and is stored at %p\n", 
+		k, (void *)&k);
+	printf("ptr has the value %p and is stored at %p\n", 
+		ptr, (void *)&ptr);
+	printf("The value of the integer pointed to by ptr is %d\n\n", 
+		*ptr);
+
+	return 0;
+}
